9b0aee430e5b13a79cd29d0ff301997e6f422d1a
fahyik yong <fahyik.yong@spendesk.com>
2021-04-05 08:54:47 +0200

    stripe

--- doodle/__tests__/doodle.test.ts
+++ doodle/__tests__/doodle.test.ts
@@ -1,9 +1,54 @@
-import { sum } from "../doodle";
+import { buildCheckLimit } from "../doodle";
 
-describe("sum()", () => {
-  describe("when given two integers", () => {
-    it("returns the correct sum", () => {
-      expect(sum(1, 2)).toEqual(1 + 2);
+describe("buildCheckLimit()", () => {
+  describe("when given an empty store", () => {
+    const dataStore = new Map<string, Date[]>();
+    const checkLimit = buildCheckLimit(dataStore);
+    describe("and a new request", () => {
+      it("returns true", () => {
+        const result = checkLimit.checkLimit("randomCustomer");
+        expect(result).toEqual(true);
+      });
+    });
+  });
+
+  describe("when given datastore with 5 previous requests within 2 seconds", () => {
+    const dataStore = new Map<string, Date[]>();
+    const checkLimit = buildCheckLimit(dataStore);
+
+    dataStore.set("randomCustomer", [
+      new Date(),
+      new Date(),
+      new Date(),
+      new Date(),
+      new Date(),
+    ]);
+
+    describe("and a new request", () => {
+      it("returns false", () => {
+        const result = checkLimit.checkLimit("randomCustomer");
+        expect(result).toEqual(false);
+      });
+    });
+  });
+
+  describe("when given datastore with more than 5 requests past 2 seconds", () => {
+    const dataStore = new Map<string, Date[]>();
+    const checkLimit = buildCheckLimit(dataStore);
+
+    dataStore.set("randomCustomer", [
+      new Date("2020-01-01"),
+      new Date("2020-01-01"),
+      new Date("2020-01-01"),
+      new Date("2020-01-01"),
+      new Date("2020-01-01"),
+    ]);
+
+    describe("and a new request", () => {
+      it("returns true", () => {
+        const result = checkLimit.checkLimit("randomCustomer");
+        expect(result).toEqual(true);
+      });
     });
   });
 });
--- doodle/doodle.ts
+++ doodle/doodle.ts
@@ -1,3 +1,56 @@
-export function sum(x: number, y: number) {
-  return x + y;
+export function buildCheckLimit(dataStore: Map<string, Date[]>) {
+  function checkLimit(customer: string): boolean {
+    const log = dataStore.get(customer);
+
+    if (log === undefined) {
+      return true;
+    }
+
+    const now = new Date();
+    let counter = 0;
+    for (const request of log) {
+      if (now.getTime() - request.getTime() < 2000) {
+        counter += 1;
+      }
+    }
+
+    return counter < 5;
+  }
+
+  return { checkLimit };
+}
+
+// TODO; type result
+export function buildRateLimiter(): any {
+  let counter: number = 0;
+
+  const dataStore = new Map<string, Date[]>();
+
+  const checkLimit = buildCheckLimit(dataStore);
+
+  const api = {
+    execute_endpoint: (customer: string) => {
+      return "results for " + customer;
+    },
+  };
+
+  function callApi(customer: string) {
+    if (!checkLimit.checkLimit(customer)) {
+      // return and not process
+      return;
+    }
+
+    const log = dataStore.get(customer);
+    if (log === undefined) {
+      dataStore.set(customer, [new Date()]);
+    } else {
+      log.push(new Date());
+    }
+
+    const result = api.execute_endpoint(customer);
+
+    return result;
+  }
+
+  return { callApi };
 }
