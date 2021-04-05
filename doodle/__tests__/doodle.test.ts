ebb6dda8e4120d2756644924cb38a4b5394a9221
fahyik yong <fahyik.yong@spendesk.com>
2021-04-05 10:14:29 +0200

    stripe

--- doodle/__tests__/doodle.test.ts
+++ doodle/__tests__/doodle.test.ts
@@ -1,9 +1,9 @@
-import { sum } from "../doodle";
+// import { sum } from "../doodle";
 
-describe("sum()", () => {
-  describe("when given two integers", () => {
-    it("returns the correct sum", () => {
-      expect(sum(1, 2)).toEqual(1 + 2);
-    });
-  });
-});
+// describe("sum()", () => {
+//   describe("when given two integers", () => {
+//     it("returns the correct sum", () => {
+//       expect(sum(1, 2)).toEqual(1 + 2);
+//     });
+//   });
+// });
--- doodle/doodle.ts
+++ doodle/doodle.ts
@@ -1,3 +1,51 @@
-export function sum(x: number, y: number) {
-  return x + y;
+import fetch from "node-fetch";
+
+import charges from "./charges.json";
+import customerCharges from "./customer-charges.json";
+
+export async function replayRequests(): Promise<void> {
+  const idMap = new Map<string, string>();
+
+  for (const obj of customerCharges) {
+    const { request, response } = obj;
+
+    // replace the template ids
+    let newBody = request.body;
+    let newRequestUrl = request.url;
+    idMap.forEach((value, key) => {
+      newBody = newBody.replace(key, value);
+      newRequestUrl = newRequestUrl.replace(key, value);
+    });
+
+    try {
+      const result = await fetch(`https://api.stripe.com${newRequestUrl}`, {
+        method: request.method,
+        // library doesnt allow body when method is get or head
+        body: ["GET", "HEAD"].includes(request.method) ? undefined : newBody,
+        headers: request.headers,
+      });
+      const data = await result.json();
+
+      // tracking the template ids returned
+      if (data.id) {
+        const bodyInTemplate = JSON.parse(response.body);
+        idMap.set(bodyInTemplate.id, data.id);
+      }
+      console.log(data);
+
+      assert(result.status, response.code);
+      console.log("success");
+      console.log(result.status);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
+
+function assert(expected: any, actual: any) {
+  if (expected !== actual) {
+    throw new Error("Assertion error");
+  }
+}
+
+replayRequests();
